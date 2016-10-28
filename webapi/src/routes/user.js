2994f177835034f2f332c3896422e57ac6715245
Cecilia Man <cecilia.man@corebuild.eu>
2016-10-28 16:12:54 +0300

    add user.js

---
+++ webapi/src/routes/user.js
@@ -0,0 +1,36 @@
+/**
+ * Created by cecilia.man on 28.10.2016.
+ */
+var express = require('express');
+var router = express.Router();
+var db = null;
+
+router.use(function (req, res, next) {
+    db = req.db;
+    next();
+});
+
+router.get("/users", function (req, res) {
+    if (!db) res.status(400).json("Database error");
+    db.collection("users").find().toArray(function (err, result) {
+        if (result.success) {
+            res.status(200).json(result);
+        }
+        else {
+            res.status(400).json(result);
+        }
+    })
+});
+
+router.post("/setData", function(req,res)
+{
+    var body = req.body;
+    console.log(body);
+    if (!db) res.status(400).json("Database error");
+    db.collection("users").findOneAndUpdate({username: "test2"},body,function(err,result){
+        if(err) res.status(401).json(err);
+        res.status(200).json(result);
+    })
+
+});
+module.exports = router;
--- webapi/src/server.js
+++ webapi/src/server.js
@@ -10,6 +10,7 @@ var db =require('./middleware/db').database;
 //routes
 var authRoutes =require('./routes/registration');
 var fitbitRoutes =require('./routes/fitbit');
+var userRoutes =require('./routes/user');
 
 //initialize app middleware. The argument functions will get executed before every request
 app.use(bodyParser.json());
@@ -18,12 +19,13 @@ app.use(db);
 app.use(logger);
 
 //Initialize server. This is binded to http://kairyapi.corebuild.eu
-app.listen(8082, function () {
-    console.log("stareted on port 8082")
+app.listen(3000, function () {
+    console.log("stareted on port 3000")
 });
 
 app.use(authRoutes)
 app.use(fitbitRoutes)
+app.use(userRoutes)
 
 app.get("/", function (req, res) {
     res.status(200).json("Hello")
