db8c9c7b0091076052a65d2f3471413f11397971
cristian.jora <cristian.jora@corebuild.eu>
2016-11-10 22:28:23 +0200

    Error handling

--- server/models/payment.js
+++ server/models/payment.js
@@ -8,9 +8,9 @@ module.exports = function (Payment) {
     var senderId = ctx.instance.senderId;
     var receiverId = ctx.instance.receiverId;
     if (!senderId || !receiverId) {
-      console.log(ctx.res)
-      next(getError("No sender or receiver provided!"));
+      return next(getError("No sender or receiver provided!"));
     }
+
     customerModel.findById(senderId, function (err, sender) {
       treatError(err,sender);
       findReceiverAndUpdateBalances(sender);
@@ -18,6 +18,9 @@ module.exports = function (Payment) {
     function findReceiverAndUpdateBalances(sender) {
       customerModel.findById(receiverId, function (err, receiver) {
         treatError(err,receiver)
+        if(!receiver || !sender){
+          return next(getError("No sender or receiver provided!"));
+        }
         receiver.balance -= ctx.instance.amount;
         sender.balance += ctx.instance.amount;
         sender.save(function (err, result) {
@@ -33,22 +36,20 @@ module.exports = function (Payment) {
     }
     function treatError(err,result) {
       if (err) {
-        next(getError(err));
+        return next(getError(err));
       }
       if(!result || result==null){
-        next(getError("Could find one or more entities"))
+        return next(getError("Could find one or more entities"))
       }
     }
-    next();
   });
   Payment.observe('after save',function updateBalances(ctx, next){
-      console.log(ctx.instance,"after saveeee");
       ctx.instance.user=user;
       next();
   })
   function getError(message) {
-    var err = new Error(message);
-    err.statusCode = 400;
+    var err=new Error(message);
+    err.status=400;
     return err;
   }
 
