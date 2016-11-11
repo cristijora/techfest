31c335d7c91528e53e7d495b6456596aad8b6b7a
cristian.jora <cristian.jora@corebuild.eu>
2016-11-11 16:13:18 +0200

    Added last payments

--- server/models/customer.js
+++ server/models/customer.js
@@ -1,3 +1,35 @@
 'use strict';
-module.exports = function(Customer) {
-};
+module.exports = function (Customer) {
+
+  Customer.afterRemote('login', function (ctx, result, next) {
+    result.lastPayments = [];
+    var paymentModel = Customer.app.models.payment;
+    paymentModel.find({
+      order:'date DESC',
+      where: {senderId: result.userId},
+      limit:3,
+      }, function (err, payments) {
+      if(err || result==null || result==undefined){
+        treatError(err);
+      }
+      result.lastPayments=payments;
+      next();
+    })
+
+    function treatError(err,result) {
+      if (err) {
+        return next(getError(err));
+      }
+      if(!result){
+        return next(getError("Could find one or more entities"))
+      }
+    }
+
+  });
+
+  function getError(message) {
+    var err=new Error(message);
+    err.status=400;
+    return err;
+  }
+}
