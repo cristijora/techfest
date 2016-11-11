c8f02944c6c10b7ecac56f3a78896343f31c15fd
Anastasia <Anastasia>
2016-11-11 18:19:59 +0200

    include challenges on user

--- server/models/customer.js
+++ server/models/customer.js
@@ -26,7 +26,27 @@ module.exports = function (Customer) {
     }
 
   });
+  Customer.afterRemote('findById', function(ctx,result,next){
+    var challengeModel = Customer.app.models.challenge;
+    console.log(result);
+    result.challenges=[];
+    challengeModel.find({where:{userId:ctx.id}},function(err,challenges){
+      if(err || result==null || result==undefined){
+        treatError(err);
+      }
+      result.challenges=challenges;
+      next();
+    })
 
+    function treatError(err,result) {
+      if (err) {
+        return next(getError(err));
+      }
+      if(!result || result==null){
+        return next(getError("Could find one or more entities"))
+      }
+    }
+  });
   function getError(message) {
     var err=new Error(message);
     err.status=400;
